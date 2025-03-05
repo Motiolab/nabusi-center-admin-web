61d19355d43bde509cb992a98312f45cec231f22
권준학 <kwonjunhak@gwonjunhags-MacBook-Pro.local>
2025-03-05 21:31:07 +0900

    added member cancel reservation

--- src/shared/utils/format/reservationStatusToKr.ts
+++ src/shared/utils/format/reservationStatusToKr.ts
@@ -3,7 +3,8 @@ const ReservationStatusToKr = (value: string) => {
         case "INAPP_RESERVATION": return "인앱 예약";
         case "ADMIN_RESERVATION": return "관리자 예약";
         case "ONSITE_RESERVATION": return "현장 예약";
-        case "CANCELED_RESERVATION": return "예약 취소";
+        case "MEMBER_CANCELED_RESERVATION": return "예약 취소";
+        case "ADMIN_CANCELED_RESERVATION": return "예약 취소";
         case "CHECK_IN": return "출석";
         case "ABSENT": return "결석";
         default: return value;
